bce30c464c39469f6b4c66ba552463611b336852
Vlad Beglik <58860425+LeWsha01@users.noreply.github.com>
2021-04-19 15:35:33 +0300

    Add files via upload
    DZ2

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int max = 0;
+			int count = 0;
+			/*string str = "assqwedddddqwertyuioasd";*/
+			string str = args[0];
+			for (int i = 0; i < str.Length - 1; i++)
+			{
+				
+				if (str[i] != str[i + 1])
+				{
+					count++;
+				}
+				else
+				{
+					count = 0;
+				}
+
+				if (max < count)
+				{
+					max = count;
+				}
+
+			}
+
+			Console.WriteLine(max.ToString());
+		}
+	}
+}
