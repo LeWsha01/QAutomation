c09fa66b8f5769982548c0ab9757a2cc0784b379
Vlad Beglik <58860425+VladBeglik@users.noreply.github.com>
2021-05-24 17:00:06 +0300

    Add files via upload

---
+++ university/Interfaces/IServiceInformation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace University.Interfaces
+{
+	public interface IServiceInformation
+	{
+		void GetAllProfessors();
+		void GetAllCourses();
+	}
+}
