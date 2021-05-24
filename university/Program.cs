d10662640ce4a161f6b38e91a49381f6d5d7afb1
Vlad Beglik <58860425+VladBeglik@users.noreply.github.com>
2021-05-24 16:54:55 +0300

    Add files via upload

---
+++ university/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using University.Interfaces;
+using University.Services;
+
+namespace University
+{
+	public class Program
+	{
+		public static IServiceInformation serviceInformation { get; set; }
+		public static void Main(string[] args)
+		{
+
+			serviceInformation = new ServiceInfomation();
+			//var result = serviceInformation.GetAllProfessors();
+			serviceInformation.GetAllProfessors();
+
+		}
+	}
+}
